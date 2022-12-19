918be91ee5088fd62ee831d702b2a61f80cf7999
Olyamba <olyailinskaya@yandex.ru>
2022-12-19 04:13:16 +0300

    damn

---
+++ src/main/java/olyamba/Main.java
@@ -0,0 +1,10 @@
+package olyamba;
+
+import olyamba.bot.Bot;
+
+public class Main {
+    public static void main(String[] args) {
+        Bot bot = new Bot();
+        bot.serve();
+    }
+}
---
+++ src/main/java/olyamba/bot/Bot.java
@@ -0,0 +1,26 @@
+package olyamba.bot;
+
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.CallbackQuery;
+import com.pengrad.telegrambot.model.InlineQuery;
+import com.pengrad.telegrambot.model.Message;
+import com.pengrad.telegrambot.model.Update;
+
+public class Bot {
+    private final TelegramBot bot = new TelegramBot(System.getenv("BOT_TOKEN"));
+
+    public void serve() {
+        bot.setUpdatesListener(updates -> {
+            updates.forEach(this::process);
+            return UpdatesListener.CONFIRMED_UPDATES_ALL;
+        });
+    }
+
+    private void process(Update update) {
+        Message message = update.message();
+        CallbackQuery callbackQuery = update.callbackQuery();
+        InlineQuery inlineQuery = update.inlineQuery();
+        System.out.println("damn");
+    }
+}
